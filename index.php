c2fa00c968b78cc2633f1384653c0617428117ca
SebCxR <sebastien.gervais96@sfr.fr>
2013-12-19 22:03:54 +0100

    index

---
+++ index.php
@@ -0,0 +1,13 @@
+ï»¿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" SYSTEM "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>	
+<meta name="generator" content="Dev-PHP 2.6.0"/>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+
+
+<html>
+  <body>
+    <h1>Hello World</h1>
+    <p> Mais keskon va faire là dedans ??? </p>
+  </body>
+</html>
